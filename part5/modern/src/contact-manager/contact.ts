c2bf30a7da8712dea4c7457c4bf1439d554ef692
Raja Mani <rmani@transperfect.com>
2016-09-09 21:22:57 -0700

    Added more types into the classes

--- part5/modern/src/contact-manager/contact.ts
+++
@@ -1,8 +0,0 @@
-export interface IContact {
-    id: number;
-    firstName: string;
-    lastName: string;
-    email: string;
-    phoneNumber: string;
-    birthDate: Date;
-}
--- part5/modern/src/contact-manager/no-selection.ts
+++ part5/modern/src/contact-manager/no-selection.ts
@@ -1,3 +1,3 @@
 export class NoSelection {
-    message = "Please Select a Contact.";
+    message = "Please Select a Contact from Contact List.";
 }
